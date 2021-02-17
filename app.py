296a5c2e0c7ca7f8fb1a18728a4ec0db0c2582f8
osamaasim <osamaasim1409@gmail.com>
2021-02-17 18:45:45 +0300

    Products table created

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask
+# imports - standard imports
+import os
+import json
+import sqlite3
+
+from flask import Flask, url_for, request, redirect
+from flask import render_template as render
+
+# global constants
+DATABASE_NAME = 'inventory.sqlite'
+
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'database', DATABASE_NAME),
+)
+
 
 @app.route("/")
 def home():
